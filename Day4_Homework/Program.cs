ed0274d81f7f399155935715777e335dadefdbcc
McShi <rashida.mcwilliams@gmail.com>
2017-09-28 15:35:29 -0400

    Two exercises completed

--- Day4_Homework/Program.cs
+++ Day4_Homework/Program.cs
@@ -10,6 +10,41 @@ namespace Day4_Homework
     {
         static void Main(string[] args)
         {
+            //string firstName = "Rashida";
+            //Console.WriteLine("Please enter your first name.");
+            //string firstName = Console.ReadLine();
+            //Console.WriteLine(firstName.Length);
+
+            //string lastName = "McWilliams";
+            //Console.WriteLine("Please enter your last name.");
+            //string lastName = Console.ReadLine();
+            //Console.WriteLine(lastName.Length);
+
+            //if (firstName.Length > lastName.Length)
+            //{
+            //    Console.WriteLine("First is longer!");
+            //}
+            //else if (firstName == lastName)
+            //{
+            //    Console.WriteLine("Samesies.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Last must be longer!");
+            //}
+
+            string aName = "Ebony";
+            string bName = "Ivory";
+
+            if (aName == bName)
+            {
+                Console.WriteLine("The names are the same!");
+            }
+            else
+            {
+                Console.WriteLine("The names are different. Boo.");
+            }
+
         }
     }
 }
